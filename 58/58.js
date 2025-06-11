a2580e2d1f49717be41d5bf2636ab50b7b3c02bc
Supriyo Barua <59415473+supriyobaruab@users.noreply.github.com>
2025-06-11 10:49:53 +0600

    solved 58

--- 58/58.js
+++ 58/58.js
@@ -2,4 +2,11 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function (s) {};
+var lengthOfLastWord = function (s) {
+  let trimed = s.trim();
+  let sliced = trimed.split(" ");
+  //   return sliced[s.length - 1];
+  console.log(sliced[sliced.length - 1].length);
+};
+s = "   fly me   to   the moon  ";
+lengthOfLastWord(s);
